7ad003ca0f314b8c988e6934dcceca09c6f25af1
Symun Hossain Shifat <saymonshifat569625@gmail.com>
2026-06-20 11:15:15 +0600

    My recipes page view details functionality added

---
+++ src/app/Dashboard/User/Myrecipe/Table.jsx
@@ -0,0 +1,61 @@
+'use client'
+import { Button, Table } from '@heroui/react'
+import { redirect } from 'next/navigation';
+
+
+import React from 'react'
+import { AiOutlineDelete } from 'react-icons/ai';
+import { FaEdit } from 'react-icons/fa';
+import { MdOutlineRemoveRedEye } from 'react-icons/md';
+
+function MYRecipespage ({Data}) { 
+    
+   
+  return (
+     <Table className='w-11/12 mx-auto m-5 p-5 text-left'>
+      <Table.ScrollContainer>
+        <Table.Content aria-label="Team members" className="min-w-[600px]">
+          <Table.Header className= 'text-center '>
+            <Table.Column isRowHeader> Recipe Name</Table.Column>
+            <Table.Column>Category</Table.Column>
+            <Table.Column>Status</Table.Column>
+            <Table.Column>Likes</Table.Column>
+            <Table.Column>Email</Table.Column>
+            <Table.Column>Action</Table.Column>
+          </Table.Header>
+          <Table.Body>
+            {
+                Data.map( recipe => (
+    
+                     <Table.Row key={recipe._id} className='font-semibold '>
+              <Table.Cell>{recipe.recipeName}</Table.Cell>
+              <Table.Cell>{recipe.category}</Table.Cell>
+              <Table.Cell>{recipe.status}</Table.Cell>
+              <Table.Cell>{recipe.likesCount}</Table.Cell>
+              <Table.Cell>{recipe.authorEmail}</Table.Cell>
+               <Table.Cell>
+                  <div className="flex items-center gap-1">
+                    <Button isIconOnly size="sm" variant="tertiary">
+                    <FaEdit size={30} />  
+                    </Button>
+                    <Button isIconOnly size="sm" variant="tertiary">
+                        <AiOutlineDelete />
+                    </Button>
+                    <Button onClick={()=> {redirect(`/Recipes/${recipe._id}`)}} isIconOnly size="sm" variant="tertiary">
+                     <MdOutlineRemoveRedEye />
+                    </Button>
+                  </div>
+                </Table.Cell>
+            </Table.Row>                ))
+                
+            }
+           
+
+          </Table.Body>
+        </Table.Content>
+      </Table.ScrollContainer>
+    </Table>
+  )
+}
+
+export default MYRecipespage 
--- src/app/Dashboard/User/Myrecipe/page.jsx
+++ src/app/Dashboard/User/Myrecipe/page.jsx
@@ -1,63 +1,18 @@
-import { GetspecificrecipesByuserEmail } from '@/lib/GetApiData/recipe'
-import { GetUserInserver } from '@/lib/GetUser/Getuserinfo'
-import { Button, Table } from '@heroui/react'
-
-
 import React from 'react'
-import { AiOutlineDelete } from 'react-icons/ai';
-import { FaEdit } from 'react-icons/fa';
-import { MdOutlineRemoveRedEye } from 'react-icons/md';
+import MYRecipespage from './Table'
+import { GetUserInserver } from '@/lib/GetUser/Getuserinfo'
+import { GetspecificrecipesByuserEmail } from '@/lib/GetApiData/recipe'
 
-async function MYRecipespage () { 
-    const User = await GetUserInserver()
-    const email =  User?.email || null 
-    const Data = await GetspecificrecipesByuserEmail(email) 
-    // console.log(Data)
+async function Homepage () {
+   const User = await GetUserInserver()
+      const email =  User?.email || null 
+      const Data = await GetspecificrecipesByuserEmail(email) 
+      // console.log(Data)
   return (
-     <Table className='w-11/12 mx-auto m-5 p-5 text-left'>
-      <Table.ScrollContainer>
-        <Table.Content aria-label="Team members" className="min-w-[600px]">
-          <Table.Header className= 'text-center '>
-            <Table.Column isRowHeader> Recipe Name</Table.Column>
-            <Table.Column>Category</Table.Column>
-            <Table.Column>Status</Table.Column>
-            <Table.Column>Likes</Table.Column>
-            <Table.Column>Email</Table.Column>
-            <Table.Column>Action</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {
-                Data.map( recipe => (
-    
-                     <Table.Row key={recipe._id} className='font-semibold '>
-              <Table.Cell>{recipe.recipeName}</Table.Cell>
-              <Table.Cell>{recipe.category}</Table.Cell>
-              <Table.Cell>{recipe.status}</Table.Cell>
-              <Table.Cell>{recipe.likesCount}</Table.Cell>
-              <Table.Cell>{recipe.authorEmail}</Table.Cell>
-               <Table.Cell>
-                  <div className="flex items-center gap-1">
-                    <Button isIconOnly size="sm" variant="tertiary">
-                    <FaEdit size={30} />  
-                    </Button>
-                    <Button isIconOnly size="sm" variant="tertiary">
-                        <AiOutlineDelete />
-                    </Button>
-                    <Button isIconOnly size="sm" variant="tertiary">
-                     <MdOutlineRemoveRedEye />
-                    </Button>
-                  </div>
-                </Table.Cell>
-            </Table.Row>                ))
-                
-            }
-           
-
-          </Table.Body>
-        </Table.Content>
-      </Table.ScrollContainer>
-    </Table>
+    <div>
+      <MYRecipespage Data={Data}></MYRecipespage>
+    </div>
   )
 }
 
-export default MYRecipespage 
+export default Homepage 
